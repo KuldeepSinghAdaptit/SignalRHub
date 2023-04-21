78d18af0dcf65482d8ac36aa332d63ae85db2640
Kuldeep Singh <kuldeep.singh@mail.vinove.com>
2023-04-21 10:16:56 +0530

    Add Code

---
+++ SignalRHub.API/Controllers/NotificationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalRHub.API.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRHub.API.Controllers
+{
+    [Route("api/notification")]
+    [ApiController]
+    public class NotificationController : Controller
+    {
+        private readonly IHubContext<NotificationHub> hub;
+        private readonly ILogger<NotificationController> logger;
+        private readonly string objBroadcastName = "TransferNotificationData";
+
+        public NotificationController(IHubContext<NotificationHub> _hub,  ILogger<NotificationController> _logger)
+        {
+            hub = _hub;
+            logger = _logger;
+        }
+
+        [HttpPost("PostSingle")]
+        public async Task<IActionResult> Post()
+        {
+
+            await hub.Clients.All.SendAsync(objBroadcastName, string.Concat("Hub Connection Successfully Established  Your GUID Code:<br/> ",Guid.NewGuid()));
+            return Ok(new { Message = "Request Complete" });
+        }
+    }
+}
---
+++ SignalRHub.API/Services/NotificationHub.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalRHub.API.Services
+{
+    public class NotificationHub:Hub
+    {
+    }
+}
